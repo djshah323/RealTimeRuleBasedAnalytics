c2b67124cf0119ad81290ae7fc233197bdf77477
= <djshah323@gmail.com>
2020-09-30 16:50:50 +0100

    cleaning up

--- src/test/java/com/realanalytics/RealAnalytics/RestControllers/RestControllerTest.java
+++ src/test/java/com/realanalytics/RealAnalytics/RestControllers/RestControllerTest.java
@@ -1,9 +1,5 @@
 package com.realanalytics.RealAnalytics.RestControllers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.time.LocalDate;
-import org.junit.jupiter.api.Test;
-
 import com.realanalytics.RealAnalytics.Events.EventService;
 
 
